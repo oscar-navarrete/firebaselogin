2cc4313dda4ea45f6813abb8c6b25c497be4ef63
Oscar Navarrete Abarca <oscar.navarrete@MacBook-Pro-de-Oscar.local>
2021-01-08 09:27:36 -0300

    Proyecto Login con Firebase

---
+++ src/components/Layout/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <router-link class="navbar-item" to="/">
+      <h3 class="title is-3">Hidroponia App</h3>
+    </router-link>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <router-link class="navbar-item" to="/">
+        Home
+      </router-link>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <router-link class="button is-primary" to="/Register">
+            <strong>Sign up</strong>
+          </router-link>
+          <router-link class="button is-light" to="/Login">
+            Log in
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from '../views/Auth/Register.vue'
+import Login from '../views/Auth/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
   }
 ]
 
